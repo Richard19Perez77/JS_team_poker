f787640126beeecca54afa8e952023185f2ae323
rick perez <richard19.perez77@yahoo.com>
2023-04-29 16:25:59 -0500

    remove draw board call

--- poker_start/scripts/gamelogic.js
+++ poker_start/scripts/gamelogic.js
@@ -12,7 +12,6 @@ function init(document) {
     tutorialImage.onload = function () {
         imagesLoaded++;
         checkImagesLoadedCount();
-        drawBoard();
     }
 
     let pageDivLocal = $(".pageDivClass");
