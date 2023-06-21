2fbe0f719bcda8e091eb0baed1a7051186e6eb49
rick perez <richard19.perez77@yahoo.com>
2023-06-21 01:20:58 -0500

    bdd test for taking hc for 2k

--- poker_start/test/bdd1.js
+++ poker_start/test/bdd1.js
@@ -17,6 +17,7 @@ function runBDDTests() {
     highCardNull();
     highCardPlayed();
     highCardReplacedForHC();
+    highCardReplacedFor2K();
     testTextArea.value += "\nBDD Tests Complete";
 }
 
@@ -226,20 +227,129 @@ function highCardReplacedForHC() {
     testTextArea.value += `\n\u274C highCardReplacedForHC() failed result=${hcSlotCard.value} exp=14`;
 }
 
-// function highCardReplacedFor2K() {
-//     // given a card in the hc slot
-//     // and I can use that card to gain a lot of points
-//     // when I have another high card
-//     // I should play that high card
+function highCardReplacedFor2K() {
+    // given a card in the hc slot is K
+    // when I can replace it for a pair even if lower
+    // Then replace the K with the to be paired card, this incstance a K
+    newGameClicked();
 
-//     // set hcSlotCard with K
-//     // replace K with Q for second K in hand
-//     if (hcSlotCard.value === 13) {
-//         testTextArea.value += "\n\u2705 highCardPlayed() Passed";
-//         return;
-//     }
+    player1Cards = [];
+    let suit = 0;
+    let value = 2;
+    let path1 = getCardImagePath(suit, value);
+    let card1 = new Card(suit, value, path1);
+    // card1.bitmap.src = card1.imagePath;
+    player1Cards[0] = card1;
 
-//     testTextArea.value += `\n\u274C highCardPlayed() failed result=${hcSlotCard.value} exp=13`;
+    value = 3;
+    let path2 = getCardImagePath(suit, value);
+    let card2 = new Card(suit, value, path2);
+    // card2.bitmap.src = card2.imagePath;
+    player1Cards[1] = card2;
 
-//     // validate hand has 2K of value 13
-// }
+    value = 4;
+    let path3 = getCardImagePath(suit, value);
+    let card3 = new Card(suit, value, path3);
+    // card3.bitmap.src = card3.imagePath;
+    player1Cards[2] = card3;
+
+    value = 5;
+    let path4 = getCardImagePath(suit, value);
+    let card4 = new Card(suit, value, path4);
+    // card4.bitmap.src = card4.imagePath;
+    player1Cards[3] = card4;
+
+    value = 6;
+    let path5 = getCardImagePath(suit, value);
+    let card5 = new Card(suit, value, path5);
+    // card5.bitmap.src = card5.imagePath;
+    player1Cards[4] = card5;
+
+    value = 7;
+    let path6 = getCardImagePath(suit, value);
+    let card6 = new Card(suit, value, path6);
+    // card7.bitmap.src = card5.imagePath;
+    player1Cards[5] = card6;
+
+    suit = 1;
+    value = 14;
+    let path7 = getCardImagePath(suit, value);
+    let card7 = new Card(suit, value, path7);
+    // card8.bitmap.src = card7.imagePath;
+    player1Cards[6] = card7;
+
+    findHCcard();
+
+    if (hcSlotCard.value === 14) {
+
+        playerTurn++;
+
+        player1Cards = [];
+        suit = 2;
+        value = 2;
+        path1 = getCardImagePath(suit, value);
+        card1 = new Card(suit, value, path1);
+        // card1.bitmap.src = card1.imagePath;
+        player2Cards[0] = card1;
+
+        value = 3;
+        path2 = getCardImagePath(suit, value);
+        card2 = new Card(suit, value, path2);
+        // card2.bitmap.src = card2.imagePath;
+        player2Cards[1] = card2;
+
+        value = 4;
+        path3 = getCardImagePath(suit, value);
+        card3 = new Card(suit, value, path3);
+        // card3.bitmap.src = card3.imagePath;
+        player2Cards[2] = card3;
+
+        value = 5;
+        path4 = getCardImagePath(suit, value);
+        card4 = new Card(suit, value, path4);
+        // card4.bitmap.src = card4.imagePath;
+        player2Cards[3] = card4;
+
+        value = 6;
+        path5 = getCardImagePath(suit, value);
+        card5 = new Card(suit, value, path5);
+        // card5.bitmap.src = card5.imagePath;
+        player2Cards[4] = card5;
+
+        suit = 4;
+        value = 14;
+        path6 = getCardImagePath(suit, value);
+        card6 = new Card(suit, value, path6);
+        // card7.bitmap.src = card5.imagePath;
+        player2Cards[5] = card6;
+
+        suit = 3;
+        value = 13;
+        path7 = getCardImagePath(suit, value);
+        card7 = new Card(suit, value, path7);
+        // card8.bitmap.src = card7.imagePath;
+        player2Cards[6] = card7;
+
+        // should have played the queen, now play the king over it
+        findHCcard()
+
+        let matches = 0;
+        player2Cards.forEach(function (card) {
+            if (card.value === 14) {
+                matches++;
+            }
+        });
+
+        if (matches === 2) {
+            testTextArea.value += "\n\u2705 highCardReplacedFor2K() Passed";
+            return;
+        } else {
+            testTextArea.value += `\n\u274C highCardReplacedFor2K() failed result=${matches} exp=2`;
+            return;
+        }
+
+    } else {
+        testTextArea.value += `\n\u274C highCardReplacedFor2K() failed result=${hcSlotCard.value} exp=14`;
+        return;
+    }
+}
