08cbe75d0344d25947492254c36ea5c4f4cb81f9
Rick <58344001+Richard19Perez77@users.noreply.github.com>
2023-07-01 11:46:44 -0500

    comments for card.js
    Add comments for card function

--- poker_start/scripts/card/card.js
+++ poker_start/scripts/card/card.js
@@ -1,3 +1,9 @@
+// Card function is used to set properties of a card.
+// @param this.suit - The suit of the card
+// @param this.value - The value of the card
+// @param this.imagePath - The path of the card image
+// @param this.bitmap - The bitmap of the path's image in a new Image()
+// @param this.bitmap.onload - The callback when the image is loaded
 function Card(s, v, p) {
     this.suit = s;
     this.value = v;
